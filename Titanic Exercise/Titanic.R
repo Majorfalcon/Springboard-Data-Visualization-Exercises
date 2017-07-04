57e462e509ab127a25979bc65555d224ec747dfc
Andre Velez <drdre2266@gmail.com>
2017-07-04 17:31:06 -0500

    Clarification on comments

--- Titanic Exercise/Titanic.R
+++ Titanic Exercise/Titanic.R
@@ -5,7 +5,7 @@ str(titanic)
 ggplot(titanic, aes(x = Pclass, fill = Sex)) + 
   geom_bar(position = "dodge")
 
-# 3 - Plot 2, add facet_grid() layer
+# 3 - Plot 2, add facet_grid() layer to predict survival rate based on sex and class variables
 ggplot(titanic, aes(x = Pclass, fill = Sex)) + 
   geom_bar(position = "dodge") +
   facet_grid(. ~ Survived)
@@ -14,6 +14,7 @@ ggplot(titanic, aes(x = Pclass, fill = Sex)) +
 posn.jd <- position_jitterdodge(0.5, 0, 0.6)
 
 # 5 - Plot 3, but use the position object from instruction 4
+# Age set to the y axis to compare the addtional effects Age had on survival rate
 ggplot(titanic, aes(x = Pclass, y = Age, col = Sex)) + 
   geom_point(size = 3, alpha = 0.5, position = posn.jd) +
   facet_grid(. ~ Survived)
